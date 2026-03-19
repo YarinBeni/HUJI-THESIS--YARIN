fbaafa9aed8066defe8b541663d6a62b72e2adb1
yarin.b <yarin.b@taboola.com>
2026-03-19 16:28:15 +0200

    Fix cluster repo path in bias check Slurm script

--- v_1/src/bias_check/run_bias_check.sh
+++ v_1/src/bias_check/run_bias_check.sh
@@ -16,7 +16,7 @@ echo "Start: $(date)"
 # --- Environment setup ---
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate thesis
-cd ~/projects/lititure-review
+cd ~/projects/HUJI-THESIS--YARIN
 git pull origin main
 
 # Install/update dependencies
